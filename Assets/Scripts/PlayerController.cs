5e2f80873c753d197773f5f735cdd7ef6e68c4e9
ign1ka <nilesdorn@gmail.com>
2023-11-09 15:50:30 -0800

    Basic player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 10.0f;
+    public float dashForce = 50.0f;
+ 
+    private Rigidbody2D rb;
+    private Collider2D c2D;
+
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        c2D = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+
+
+
+    
+
+        // Handle player movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(horizontalInput, 0) * moveSpeed;
+
+        // Apply movement to the player
+        if (IsGrounded())
+        {
+            rb.velocity = new Vector2(movement.x, rb.velocity.y);
+        }
+
+        // Check for jump input
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+
+        // Check for dashing input
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            Dash();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+  
+    }
+
+    private void Jump()
+    {
+        // Apply an upward force for jumping
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void Dash()
+    {
+        // Calculate the direction from the player to the mouse cursor
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dashDirection = (mousePos - transform.position).normalized;
+
+        // Apply a force in the direction opposite to the mouse cursor
+        rb.AddForce(-dashDirection * dashForce, ForceMode2D.Impulse);
+    }
+
+    private bool IsGrounded()
+    {
+        return c2D.IsTouchingLayers(); //Can also use .IsTouchingLayera(LayerMask.GetMask("Environment"))
+    }
+}
